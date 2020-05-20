9efe3a4a467df8a90fd1b5c1742f0d874287ae56
bvera88 <59676112+bvera88@users.noreply.github.com>
2020-05-20 00:05:52 -0400

    update

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -6,17 +6,19 @@ var tbody = d3.select("tbody");
 // YOUR CODE HERE!
 console.log(data);
 
-data.forEach(function(aliensreport) {
-    console.log(aliensreport);
-    var row = tbody.append("tr");
-    Object.entries(aliensreport).forEach(function([key, value]) {
-        console.log(key, value);
+// data.forEach(function(aliensreport) {
+//     console.log(aliensreport);
+//     var row = tbody.append("tr");
+//     Object.entries(aliensreport).forEach(function([key, value]) {
+//         console.log(key, value);
 
-        var cell = row.append("td");
-        cell.text(value);
-    });
-});
+//         var cell = row.append("td");
+//         cell.text(value);
+//     });
+// });
 
+
+// refactoring the method here
 data.forEach(aliensreport => {
     var row = tbody.append("tr");
     row.append ("td").text(aliensreport.datetime);
@@ -26,40 +28,28 @@ data.forEach(aliensreport => {
     row.append ("td").text(aliensreport.shape);
     row.append ("td").text(aliensreport.durationMinutes);
     row.append ("td").text(aliensreport.comments);
-}) 
+});
 
-var dateInputText = d3.select("#datetime")
-var button = d3.select("filter-btn")
+var button = d3.select("#filter-btn");
 
-// filter data with date that the user inputs
-function clickSelect(){
-    //don't refresh the page!
+var form = d3.select("#form");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+
     d3.event.preventDefault();
-    //print the value that was input
-    console.log(dateInputText.property("value"));
-    //create a new table showing only the filterd data
-    var filteredData = data.filter(sighting => sighting.datetime===dateInputText.property("value"))
-    //display the new table
-    displayData(filteredData);
-}
+    var inputfield =d3.select("#datetime");
+    var inputValue = inputfield.propery("value");
 
-// event listener to handle change and click
-dateInputText.on("change", clickSelect)
-
-// var button = d3.select("#filter-btn");
-
-// var inputfield =d3.select("#datetime");
-
-// function handleClick() {
-//     console.log("filter button clicked");
-//     console.log(d3.event.target);
-// }
+    console.log(inputValue)
+    d3.select("tbody").text(inputValue)  
+};
 
 // inputfield.on("field",function() {
 //     var newText =d3.event.target.value;
 //     console.log(newText);
 // })
 
-// inputfield.on("change",handleClick)
-
-// d3.event.preventDefault();
+// inputfield.on("change",handleClick);
